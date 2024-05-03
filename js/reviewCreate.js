b5c4bcd543d7903fda02190e2891fd0063d1cae6
parkchowon <70216263+parkchowon@users.noreply.github.com>
2024-05-03 12:20:55 +0900

    update:localStorage 넣기

--- js/reviewCreate.js
+++ js/reviewCreate.js
@@ -4,6 +4,38 @@ const userName = document.querySelector(".review-name");
 const comment = document.querySelector(".review-comment");
 const registerBtn = document.querySelector(".review-btn");
 
+//버튼 클릭 시
 registerBtn.addEventListener("click", () => {
-  console.log("클릭" + userName.value, comment.value, Checked);
+  verificateInput();
 });
+
+//Enter 누를 시
+comment.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    verificateInput();
+  }
+});
+
+//유효성 검사 함수
+const verificateInput = () => {
+  if (userName.value == "") {
+    alert("Please Enter the name");
+  } else if (comment.value == "") {
+    alert("Please Enter the comment");
+  } else {
+    registReview();
+  }
+};
+
+//리뷰 등록 함수
+const registReview = () => {
+  console.log("클릭" + userName.value, comment.value, Checked);
+  const review = {
+    name: userName.value,
+    comment: comment.value,
+    star: Checked,
+    password: null,
+  };
+  localStorage.setItem(userName.value, JSON.stringify(review));
+  console.log(JSON.parse(localStorage.getItem(userName.value)));
+};
